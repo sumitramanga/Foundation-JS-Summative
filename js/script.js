8995ac0cd670b6ab529ec0f399ccdb3f080fce19
sumitra manga <sumitramanga@gmail.com>
2018-06-13 09:23:15 +1200

    Added SASS

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
-// Accommodation data
+(function() {
+  // Accommodation data
+  var accommodation = {
+    amount: 4,
 
-var accommodation = {
-  amount: 4,
+    hostel: {
+      cost: 30,
+      people: [1, 2, 3, 4, 5, 6],
+      nights: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
 
-  hostel: {
-    cost: 30,
-    people: [1, 2, 3, 4, 5, 6],
-    nights: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  },
+    motel: {
+      cost: 90,
+      people: [2, 3, 4],
+      nights: [3, 4, 5, 6, 7, 8, 9, 10]
+    },
 
-  motel: {
-    cost: 90,
-    people: [2, 3, 4],
-    nights: [3, 4, 5, 6, 7, 8, 9, 10]
-  },
+    hotel: {
+      cost: 157,
+      people: [1, 2],
+      nights: [1, 2, 3, 4, 5]
+    },
 
-  hotel: {
-    cost: 157,
-    people: [1, 2],
-    nights: [1, 2, 3, 4, 5]
-  },
+    house: {
+      cost: 240,
+      people: [1, 2, 3, 4],
+      nights: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    },
+  }
 
-  house: {
-    cost: 240,
-    people: [1, 2, 3, 4],
-    nights: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  },
-}
+  // Page piling script
+    $(document).ready(function() {
+      $('#fullpage').fullpage({
+        verticalCentered: true,
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        sectionsColor: ['#EAE1C0', '#DE564B', '#EAE1C0'],
+        bgSize: ['cover', 'cover', 'cover'],
+        slidesNavigation: true,
+      });
+
+      $('#exploreBtn').click(function(e){
+        e.preventDefault();
+        $.fn.fullpage.setScrollingSpeed(2100);
+        $.fn.fullpage.moveSectionDown();
+      });
+    }); // DOCUMENT READY ENDS
+
+}()); // IIFE ENDS

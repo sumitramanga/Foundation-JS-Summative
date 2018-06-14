ca3ee09e153931a16b3e55ec85ca05e1428a21c7
sumitra manga <sumitramanga@gmail.com>
2018-06-14 15:42:58 +1200

    Created Check in/out page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 (function() {
+  var controlArrows = document.getElementsByClassName('fp-controlArrow');
+
   // Accommodation data
   var accommodation = {
     amount: 4,
@@ -43,6 +45,29 @@
         $.fn.fullpage.setScrollingSpeed(2100);
         $.fn.fullpage.moveSectionDown();
       });
+
+      $( function() {
+      $( "#datepicker" ).datepicker();
+    } );
     }); // DOCUMENT READY ENDS
+
+//FORM VALIDATOR
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
 
 }()); // IIFE ENDS

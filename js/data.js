5f57ddf0491cea45093e1e42944040d6eecc05c1
sumitra manga <sumitramanga@gmail.com>
2018-06-15 12:52:35 +1200

    Created map page and results page

---
+++ js/data.js
@@ -0,0 +1,28 @@
+// Accommodation data
+var accommodation = {
+  amount: 4,
+
+  hostel: {
+    cost: 30,
+    people: [1, 2, 3, 4, 5, 6],
+    nights: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  },
+
+  motel: {
+    cost: 90,
+    people: [2, 3, 4],
+    nights: [3, 4, 5, 6, 7, 8, 9, 10]
+  },
+
+  hotel: {
+    cost: 157,
+    people: [1, 2],
+    nights: [1, 2, 3, 4, 5]
+  },
+
+  house: {
+    cost: 240,
+    people: [1, 2, 3, 4],
+    nights: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  },
+}
--- js/script.js
+++ js/script.js
@@ -1,73 +1,59 @@
 (function() {
   var controlArrows = document.getElementsByClassName('fp-controlArrow');
 
-  // Accommodation data
-  var accommodation = {
-    amount: 4,
-
-    hostel: {
-      cost: 30,
-      people: [1, 2, 3, 4, 5, 6],
-      nights: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    },
-
-    motel: {
-      cost: 90,
-      people: [2, 3, 4],
-      nights: [3, 4, 5, 6, 7, 8, 9, 10]
-    },
-
-    hotel: {
-      cost: 157,
-      people: [1, 2],
-      nights: [1, 2, 3, 4, 5]
-    },
-
-    house: {
-      cost: 240,
-      people: [1, 2, 3, 4],
-      nights: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    },
-  }
-
   // Page piling script
     $(document).ready(function() {
       $('#fullpage').fullpage({
         verticalCentered: true,
-        anchors: ['firstPage', 'secondPage', '3rdPage'],
-        sectionsColor: ['#EAE1C0', '#DE564B', '#EAE1C0'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', 'fourthPage', 'fifthPage'],
+        sectionsColor: ['white', '#D66761', 'white', '#D66761'],
         bgSize: ['cover', 'cover', 'cover'],
         slidesNavigation: true,
       });
 
       $('#exploreBtn').click(function(e){
         e.preventDefault();
-        $.fn.fullpage.setScrollingSpeed(2100);
+        $.fn.fullpage.setScrollingSpeed(1000);
         $.fn.fullpage.moveSectionDown();
       });
 
-      $( function() {
-      $( "#datepicker" ).datepicker();
-    } );
+      $('#submitBtn').click(function(e){
+        e.preventDefault();
+        $.fn.fullpage.setScrollingSpeed(1000);
+        $.fn.fullpage.moveSectionDown();
+      });
+
+      $( function(){
+        $( "#datepicker" ).datepicker();
+      });
     }); // DOCUMENT READY ENDS
 
-//FORM VALIDATOR
-    (function() {
-      'use strict';
-      window.addEventListener('load', function() {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-          form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-          }, false);
+    // SETTING UP MAP ---------------------------------------------------------------
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic3VtaXRyYW0iLCJhIjoiY2ppM240OTk2MDBhbTNxbzQyeXQ3NjcxNCJ9.AI-7xSWsKG5uHfGk9jFJkA';
+        var map = new mapboxgl.Map({
+          container: 'map', // container id
+          style: 'mapbox://styles/sumitram/cji3p2cwm0s6r2smz01nlidgc', // stylesheet location
+          center: [174.780651, -41.278932], // starting position [lng, lat]
+          zoom: 17 // starting zoom
         });
-      }, false);
-    })();
+
+// //FORM VALIDATOR
+//     (function() {
+//       'use strict';
+//       window.addEventListener('load', function() {
+//         // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//         var forms = document.getElementsByClassName('needs-validation');
+//         // Loop over them and prevent submission
+//         var validation = Array.prototype.filter.call(forms, function(form) {
+//           form.addEventListener('submit', function(event) {
+//             if (form.checkValidity() === false) {
+//               event.preventDefault();
+//               event.stopPropagation();
+//             }
+//             form.classList.add('was-validated');
+//           }, false);
+//         });
+//       }, false);
+//     })();
 
 }()); // IIFE ENDS

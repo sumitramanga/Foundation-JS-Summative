f97815de8115a0ead4cd8293e1d8e7c426b946a0
sumitra manga <sumitramanga@gmail.com>
2018-06-23 17:41:03 +1200

    Added validation to dates slide. Cleaned up code. Fixed error of diffDays.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 (function() {
   var controlArrows = document.getElementsByClassName('fp-controlArrow');
 
-  // Getting the users options from the SECOND section
+  // Getting the users options from the SECOND section -------------------------
   var getGuests = document.getElementById('getGuests');
 
   var getCheckInDate = document.getElementById('getCheckIn');
   var getCheckOutDate = document.getElementById('getCheckOut');
-  var daysText = document.getElementById('daysText');
+  var nightsSelected = document.getElementById('nightsSelected');
+  var nightsText = document.getElementById('nightsText');
 
   var getMeals = document.getElementById('getMealOption');
 
-  var errorMessage = document.createElement('div');
   var getSubBtn = document.getElementById('submitBtn');
+  var errorMessage = document.createElement('div');
 
-  // The button which pushes the details entered into an array
+  // The button which pushes the details entered into an array -----------------
   var browseBtn = document.getElementById('browseBtn');
 
-  // Getting the results elements from the FIFTH section
+  // Getting the results elements from the FIFTH section -----------------------
   var finalGuest = document.getElementById('guestsCount');
   var finalMealsOption = document.getElementById('mealsOption');
   var finalNights = document.getElementById('nightCount');
+
   var totalCost = document.getElementById('totalCost');
 
   var refNum = document.getElementById('refNum');
@@ -29,12 +31,14 @@
 
 // -----------------------------------------------------------------------------
 
-  // LOADING ICON
 
+  // LOADING ICON
   $(window).load(function(){
     $('.loader').fadeOut();
   });
 
+
+// -----------------------------------------------------------------------------
 
   $(document).ready(function() {
 
@@ -47,7 +51,6 @@
       slidesNavigation: true,
     });
 
-
     // Splash screen button
     $('#exploreBtn').click(function(e){
       e.preventDefault();
@@ -56,63 +59,60 @@
     });
 
 
-// -----------------------------------------------------------------------------
-
-
-
-
-
-
 
 
 // -----------------------------------------------------------------------------
 
 
-  // DATE PICKER PLUGIN
+    // DATE PICKER PLUGIN
 
-      // var $input =
-      $('.datepicker1').pickadate({
-        // editable: true,
-        clear: '',
-        min: new Date()
-        // onClose: function() { $('.datepicker').focus(); }
-      });
+    $('.datepicker1').pickadate({
+      clear: '',
+      min: new Date(),
+    });
 
-      // var picker = $input.pickadate('picker');
-      // $input.on('click', function(event) {
-      //   if (picker.get('open')) {
-      //       picker.close();
-      //   } else {
-      //       picker.open();
-      //   }
-      //   event.stopPropagation();
-      // });
-
-
-      $('.datepicker2').pickadate({
-        // editable: true,
-        clear: '',
-        min: new Date(),
-        onClose: function() {
-          // Calculate how many days in between the chosen dates
-          var startDate = Date.parse(getCheckIn.value);
-          var endDate = Date.parse(getCheckOut.value);
-          var timeDiff = endDate - startDate;
-          daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-
-          // Push the date to view on the slide
-          daysSelected.innerText = daysDiff;
-          daysText.innerText = ' Nights';
-        }
-      });
+    $('.datepicker2').pickadate({
+      clear: '',
+      min: new Date(),
+      onClose: function () {
+        calculateNights()
+      }
+    });
 
 
 // -----------------------------------------------------------------------------
 
 
-    // Form doesn't refresh the page
-    $("form").submit(function() {
-       return false;
+    function calculateNights () {
+
+      // Calculate how many days in between the chosen dates
+      var startDate = Date.parse(getCheckIn.value);
+      var endDate = Date.parse(getCheckOut.value);
+      var timeDiff = endDate - startDate;
+      daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+      // Push the date to view on the slide
+      nightsSelected.innerText = daysDiff;
+      dateErrorMsg();
+
+    }
+
+    // if (isNaN(nightsSelected)) {
+    //   nightsSelected.textContent = '0';
+    // }
+
+
+// -----------------------------------------------------------------------------
+
+
+
+
+    // Auto scroll on click the Browse Accomodation button
+    $('#browseBtn').click(function(e){
+      e.preventDefault();
+      $.fn.fullpage.setScrollingSpeed(1000);
+      $.fn.fullpage.moveSectionDown();
+      $.fn.fullpage.setAllowScrolling(false);
     });
 
     // Auto scroll on click the View Results button
@@ -124,57 +124,81 @@
     });
 
 
-    // Auto scroll on click the Browse Accomodation button
-    $('#browseBtn').click(function(e){
-      e.preventDefault();
-      $.fn.fullpage.setScrollingSpeed(1000);
-      $.fn.fullpage.moveSectionDown();
-      $.fn.fullpage.setAllowScrolling(false);
-    });
-
-
 //----------------------------------------------------------------------------
 
 
-  // FORM VALIDATION
-  window.addEventListener('load',
-  function formValidation() {
+    // FORM VALIDATION
+    window.addEventListener('load',
+    function formValidation() {
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false || daysDiff > 15 || daysDiff < 1 || daysDiff === NaN) {
-          event.preventDefault();
-          event.stopPropagation();
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false || nightsSelected.textContent > 15 || nightsSelected.textContent < 1 || nightsSelected.textContent === NaN) {
+            event.preventDefault();
+            event.stopPropagation();
 
-          // Show an error message which is created here along with fadeOut and removes from code.
-          errorMessage.className = 'errorMessage';
-          errorMessage.innerText = "Hold up! Look like something's missing. Please check to see if you've entered your details correctly.";
-          getSubBtn.after(errorMessage);
+            // Show an error message which is created here along with fadeOut and removes from code.
+            errorMessage.className = 'errorMessage';
+            errorMessage.innerText = "Hold up! Look like something's missing. Please check to see if you've entered your details correctly.";
+            getSubBtn.after(errorMessage);
 
-          $('.errorMessage').fadeOut(6000, function(){
-            $('.errorMessage').remove();
-          });
-        }
-        form.classList.add('was-validated');
-      }, false);
+            $('.errorMessage').fadeOut(6000, function(){
+              $('.errorMessage').remove();
+            });
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+
+
+    // Form doesn't refresh the page
+    $("form").submit(function() {
+       return false;
     });
-  }, false);
 
 // -----------------------------------------------------------------------------
 
 
-// Scroll section down on click when validation is complete
+    // Add a message when too less or too many nights have been selected
+    // remove the message and uncolor the inputs when the task the nights are correct
+
+    function dateErrorMsg() {
+      if (nightsSelected.textContent > 15 || nightsSelected.textContent < 1) {
+
+        var nightsErrorMsg = document.createElement('p');
+        getCheckInDate.style.border = '1px solid #DC1C0C';
+        getCheckOutDate.style.border = '1px solid #DC1C0C';
+        nightsErrorMsg.className = 'nightsErrorMsg';
+        nightsErrorMsg.style.margin = '1em';
+        nightsErrorMsg.textContent = 'You have selected an incorrect amount of days. Please try again.';
+        nightsText.after(nightsErrorMsg);
+
+      } else {
+        $('.nightsErrorMsg').remove();
+        getCheckInDate.style.borderColor = '';
+        getCheckOutDate.style.borderColor = '';
+      }
+
+    }
+
+
+// -----------------------------------------------------------------------------
+
+
+    // Scroll section down on click when validation is complete
 
     getSubBtn.addEventListener('click', scrollDown , false);
 
     function scrollDown() {
 
-      // If there is a value in the inputs the page and the nights staying is 1-15 will auto scroll
+      // If there is a value in the inputs the page and the nights staying is 1-15
+      // the page will auto scroll
       if (getGuests.validity.valueMissing === false && getMeals.validity.valueMissing === false
-      && daysDiff >= 1 && daysDiff <= 15) {
+      && nightsSelected.textContent >= 1 && nightsSelected.textContent <= 15) {
 
         $.fn.fullpage.setScrollingSpeed(1000);
         $.fn.fullpage.moveSectionDown();
@@ -185,18 +209,16 @@
       // Turn guests input from string to number
       var guestsStringToNum = parseInt(getGuests.value);
       var mealStringToNum = parseInt(getMeals.value);
-      // console.log(typeof guestsStringToNum);
 
       console.dir(typeof mealStringToNum);
       console.dir(typeof guestsStringToNum);
+
     }
-
-
 
 
 // -----------------------------------------------------------------------------
 
- // SETTING UP MAP
+    // SETTING UP MAP
 
     var token = 'pk.eyJ1Ijoic3VtaXRyYW0iLCJhIjoiY2ppbDA5ajh5MmpuMTNwb250MXR0ZWI1ayJ9.4K0zZ6PO_bnYu76JJUOmoQ';
 
@@ -309,14 +331,15 @@
 
 // -----------------------------------------------------------------------------
 
-  // Reference number on summary
+    // Reference number on summary
 
-  function createRefNum() {
-    var randomNum = Math.floor((Math.random() * 1000000) + 1);
-    refNum.innerText = '#' + randomNum;
-  }
+    function createRefNum() {
+      var randomNum = Math.floor((Math.random() * 1000000) + 1);
+      refNum.innerText = '#' + randomNum;
+    }
 
-  createRefNum();
+    createRefNum();
+
+
   }); // DOCUMENT READY ENDS
-
 }()); // IIFE ENDS
